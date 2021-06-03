5b6e86b46b82bce977d6041120cd848fc1e4a2a7
Josh Smith <josh.smith@codethink.co.uk>
2021-06-03 10:56:52 +0100

    Add e2e example with documentation

--- analysis/analysis.go
+++ analysis/analysis.go
@@ -14,6 +14,7 @@ import (
 	"strings"
 
 	"gocloud.dev/blob"
+	_ "gocloud.dev/blob/fileblob"
 	_ "gocloud.dev/blob/gcsblob"
 
 	"gocloud.dev/docstore"
--- analysis/go.mod
+++ analysis/go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	gocloud.dev v0.23.0
+	gocloud.dev/pubsub/kafkapubsub v0.23.0
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
